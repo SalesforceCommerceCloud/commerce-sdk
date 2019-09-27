f1b2c5671a119b5a99286e1ee9fdf0c5dd463b8b
Jeffrey Albert <jalbert@salesforce.com>
2019-09-27 16:35:36 -0400

    Fixed the test (#11)

--- packages/generator/test/renderer.js
+++ packages/generator/test/renderer.js
@@ -6,8 +6,11 @@ import {
   __RewireAPI__ as RendererRewireApi
 } from "../src/utils/renderer";
 import { assert } from "chai";
+var chai = require("chai");
 const fs = require("fs-extra");
 const generator = require("../src/utils/parser");
+
+chai.use(require("chai-string"));
 
 describe("copy static files tests", () => {
   it("copy those files", () => {
@@ -24,12 +27,12 @@ describe("copy static files tests", () => {
 describe("create client test code", () => {
   let model = {};
   // eslint-disable-next-line no-undef
-  before(() => {
+  beforeEach(() => {
     let ramlFile = `${__dirname}/raml/valid/site.raml`;
     return generator
       .processRamlFile(ramlFile)
       .then(res => {
-        model = res;
+        model = res.encodes;
       })
       .catch(err => {
         throw Error("Valid RAML file parsing should not fail", err);
@@ -40,7 +43,19 @@ describe("create client test code", () => {
     let tmpDir = tmp.dirSync();
     RendererRewireApi.__Rewire__("pkgDir", path.join(tmpDir.name, "pkg"));
     createClient(model);
-    // TODO: Write assertion here
+    let expected = `export default class extends BaseClient {
+      constructor() {
+          super("https://anypoint.mulesoft.com/mocking/api/v1/links/27d5ffea-96a7-447b-b595-2b0e837a20c6/s/-/dw/shop/v19_5");
+      }
+
+      getSite() {
+          this.get("/site");
+    }`;
+    let actual = fs
+      .readFileSync(path.join(tmpDir.name, "pkg/shop.js"))
+      .toString();
+
+    assert.containIgnoreSpaces(actual, expected);
     RendererRewireApi.__ResetDependency__("pkgDir");
   });
 });
