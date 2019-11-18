83e89dcf96336f21179d5a0e45b296a5c5afa463
Brian Redmond <bredmond@salesforce.com>
2019-11-18 15:42:55 -0500

    Update files with copyright header

--- packages/exchange-connector/src/bearerToken.ts
+++ packages/exchange-connector/src/bearerToken.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import { default as fetch, Response } from "node-fetch";
 
 const MULESOFT_LOGIN = "https://anypoint.mulesoft.com/accounts/login";
