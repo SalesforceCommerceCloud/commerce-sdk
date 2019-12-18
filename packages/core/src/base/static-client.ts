dba35a4fc587540ca45dac1587ee7b92bdfa102a
Brian Redmond <bredmond@salesforce.com>
2019-12-18 15:31:50 -0500

    Swap to make-fetch-happen

--- packages/core/src/base/static-client.ts
+++ packages/core/src/base/static-client.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { default as fetch, Response, RequestInit } from "node-fetch";
+import { default as fetch, Response, RequestInit } from "make-fetch-happen";
 import { Resource } from "./resource";
 import { BaseClient } from "./client";
 import { IAuthScheme } from "./auth-schemes";
