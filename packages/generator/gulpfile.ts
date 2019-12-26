66266ef085ec053d0d48cc5b5bd6ca4119e7ac06
Jeff Albert <jalbert@salesforce.com>
2019-12-26 16:46:19 -0500

    getting that integration test working

--- packages/generator/gulpfile.ts
+++ packages/generator/gulpfile.ts
@@ -33,9 +33,9 @@ const config = require("./build-config.json");
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 gulp.task("cleanTmp", (cb: any) => {
-  log.info(`Removing ${config.tmpDir} directory`);
+  log.info(`Removing ${config.renderDir} directory`);
   return;
-  // return del([`${config.tmpDir}`], cb);
+  // return del([`${config.renderDir}`], cb);
 });
 
 function search(): Promise<RestApi[]> {
@@ -57,19 +57,18 @@ function groupRamls(): Promise<void> {
   return search().then(apis => {
     return downloadRestApis(apis)
       .then(folder => {
-        config.tmpDir = folder;
+        config.inputDir = folder;
         return extractFiles(folder);
       })
       .then(async () => {
         const ramlGroups = _.groupBy(apis, api => {
           return api.categories["CC API Family"][0];
         });
-        return fs.ensureDir(`${config.tmpDir}`).then(() => {
-          return fs.writeFile(
-            path.join(`${config.tmpDir}`, RAML_GROUPS),
-            JSON.stringify(ramlGroups)
-          );
-        });
+        fs.ensureDirSync(config.inputDir);
+        return fs.writeFile(
+          path.join(`${config.inputDir}`, RAML_GROUPS),
+          JSON.stringify(ramlGroups)
+        );
       });
 
     // Group RAML files by the key, aka, bounded context/API Family
@@ -89,7 +88,7 @@ gulp.task(
     // require the json written in groupRamls gulpTask
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const ramlGroupConfig = require(path.resolve(
-      path.join(`${config.tmpDir}`, RAML_GROUPS)
+      path.join(`${config.inputDir}`, RAML_GROUPS)
     ));
     // console.log(ramlGroupConfig);
     const apiGroupKeys = _.keysIn(ramlGroupConfig);
@@ -100,22 +99,23 @@ gulp.task(
       _.map(ramlFileFromFamily, (apiMeta: RestApi) => {
         familyPromises.push(
           processRamlFile(
-            `${config.tmpDir}/${apiMeta.assetId}/${apiMeta.fatRaml.mainFile}`
+            `${config.inputDir}/${apiMeta.assetId}/${apiMeta.fatRaml.mainFile}`
           )
         );
       });
+      fs.ensureDirSync(config.renderDir);
       Promise.all(familyPromises)
         .then(values => {
           // console.log(values);
           fs.writeFileSync(
-            `${config.tmpDir}/${apiGroup}.ts`,
+            `${config.renderDir}/${apiGroup}.ts`,
             createClient(
               values.map(value => value as WebApiBaseUnitWithEncodesModel),
               apiGroup
             )
           );
           fs.writeFileSync(
-            `${config.tmpDir}/${apiGroup}.types.ts`,
+            `${config.renderDir}/${apiGroup}.types.ts`,
             createDto(
               values.map(value => value as WebApiBaseUnitWithEncodesModel)
             )
@@ -125,6 +125,6 @@ gulp.task(
           console.log(err);
         });
     }
-    fs.writeFileSync(`${config.tmpDir}/index.ts`, createIndex(apiGroupKeys));
+    fs.writeFileSync(`${config.renderDir}/index.ts`, createIndex(apiGroupKeys));
   })
 );
