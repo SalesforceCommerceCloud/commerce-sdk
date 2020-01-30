6d7077e525f6a27fcd844e89dcdd90558293232e
Ujwala <59447810+unandyala@users.noreply.github.com>
2020-01-30 13:29:16 -0500

    Organize API families and its APIs (#70)
    * Organize API families and its APIs
    
    * Addressed review comments
    
    * deleted unused apiIndex template
    
    * unit tests for render templates
    
    * unit tests for parser function

---
+++ packages/generator/test/renderer.test.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+"use strict";
+
+import fs from "fs-extra";
+import { expect } from "chai";
+import path from "path";
+import * as renderer from "../src/renderer";
+import tmp from "tmp";
+import _ from "lodash";
+
+/**
+ * Tests all the functions that are invoked while rendering templates.
+ *
+ * Note: Ideally we want a independent test for each function. As of today (01/30/2020) this is not possible with Sinon when the functions are in the same module.
+ * Sinon works by overwriting a reference to a function on an object, making it point at something else. It can't replace references to the same function in other contexts.
+ *
+ * Example: We can't mock renderApiFamily function while testing renderTemplates function. To make it work renderTemplates should invoke
+ * renderApiFamily as "module.exports.renderApiFamily(..)" which is not desired.
+ */
+it("Render Templates", () => {
+  const renderDir = tmp.dirSync();
+  const apiInputDir = path.join(__dirname, "/raml/valid");
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const apiConfig = require(path.join(
+    __dirname,
+    "/raml/valid/api-config.json"
+  ));
+  return renderer
+    .renderTemplates(renderDir.name, apiInputDir, "api-config.json")
+    .then(() => {
+      expect(fs.existsSync(path.join(renderDir.name, "index.ts"))).to.be.true;
+      const apiFamilies: string[] = _.keysIn(apiConfig);
+      apiFamilies.forEach(family => {
+        expect(fs.existsSync(path.join(renderDir.name, family, family + ".ts")))
+          .to.be.true;
+        const familyAps: any[] = apiConfig[family];
+        familyAps.forEach(api => {
+          const apiName = _.upperFirst(_.camelCase(api.name));
+          fs.existsSync(
+            path.join(renderDir.name, family, apiName, apiName + ".ts")
+          );
+          fs.existsSync(
+            path.join(renderDir.name, family, apiName, apiName + "types.ts")
+          );
+        });
+      });
+    });
+});
