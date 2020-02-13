d63d810afb33479cca79dc64276fe3e14f5b5190
Jeff Albert <jalbert@salesforce.com>
2020-02-13 17:09:51 -0500

    Fixed integration tests

--- build-config.ts
+++ build-config.ts
@@ -4,13 +4,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+require("dotenv").config();
+
 export default {
-  inputDir: `${__dirname}/apis`,
-  renderDir: "renderedTemplates",
-  apiFamily: "CC API Family",
-  exchangeSearch: 'category:"CC Visibility" = "External"',
-  apiConfigFile: "api-config.json",
-  shopperAuthClient: "Customer.ShopperCustomers",
-  shopperAuthApi: "authorizeCustomer",
-  exchangeDeploymentRegex: /production/i
+  inputDir: process.env.COMMERCE_SDK_INPUT_DIR || `${__dirname}/apis`,
+  renderDir: process.env.COMMERCE_SDK_RENDER_DIR || "renderedTemplates",
+  apiFamily: process.env.COMMERCE_SDK_API_FAMILY || "CC API Family",
+  exchangeSearch: process.env.COMMERCE_SDK_EXCHANGE_SEARCH || 'category:"CC Visibility" = "External"',
+  apiConfigFile: process.env.COMMERCE_SDK_API_CONFIG_FILE || "api-config.json",
+  shopperAuthClient: process.env.COMMERCE_SDK_SHOPPER_AUTH_CLIENT || "Customer.ShopperCustomers",
+  shopperAuthApi: process.env.COMMERCE_SDK_SHOPPER_AUTH_API || "authorizeCustomer",
+  exchangeDeploymentRegex: RegExp(process.env.COMMERCE_SDK_EXCHANGE_DEPLOYMENT_REGEX || /production/i)
 };
