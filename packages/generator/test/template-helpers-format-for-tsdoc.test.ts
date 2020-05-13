09ef15f339b43f40cbbbd34e83e098c71881da84
Brian Redmond <bredmond@salesforce.com>
2020-05-13 00:44:30 -0400

    Don't reformat

--- packages/generator/test/template-helpers-format-for-tsdoc.test.ts
+++ packages/generator/test/template-helpers-format-for-tsdoc.test.ts
@@ -15,7 +15,7 @@ describe("Test formatForTsDoc template help function", () => {
     expect(formatForTsDoc("")).to.eq("");
   });
 
-  it("returns \"test\" for \"test\"", () => {
+  it('returns "test" for "test"', () => {
     expect(formatForTsDoc("test")).to.eq("test");
   });
 
@@ -32,38 +32,52 @@ describe("Test formatForTsDoc template help function", () => {
   });
 
   it("returns escaped brackets for brackets", () => {
-      expect(formatForTsDoc("this is {escaped}")).to.be.eq("this is \\{escaped\\}");
+    expect(formatForTsDoc("this is {escaped}")).to.be.eq(
+      "this is \\{escaped\\}"
+    );
   });
 
   it("returns unescaped newlines for escapeed newlines", () => {
-      expect(formatForTsDoc("this is a newline\\n")).to.be.eq("this is a newline\n");
+    expect(formatForTsDoc("this is a newline\\n")).to.be.eq(
+      "this is a newline\n"
+    );
   });
 
   it("returns newlines for newlines", () => {
-      expect(formatForTsDoc("this is a newline\n")).to.be.eq("this is a newline\n");
+    expect(formatForTsDoc("this is a newline\n")).to.be.eq(
+      "this is a newline\n"
+    );
   });
 
   it("returns unescaped tabs for escaped tabs", () => {
-      expect(formatForTsDoc("this is a\\ttab")).to.be.eq("this is a tab");
+    expect(formatForTsDoc("this is a\\ttab")).to.be.eq("this is a tab");
   });
 
   it("returns tabs for tabs", () => {
-      expect(formatForTsDoc("this is a\ttab")).to.be.eq("this is a\ttab");
+    expect(formatForTsDoc("this is a\ttab")).to.be.eq("this is a\ttab");
   });
 
   it("returns leading whitespace for 1 space in front", () => {
-      expect(formatForTsDoc("\n this is an indented line")).to.be.eq("\n this is an indented line");
+    expect(formatForTsDoc("\n this is an indented line")).to.be.eq(
+      "\n this is an indented line"
+    );
   });
 
   it("returns leading whitespace for 3 spaces in front", () => {
-      expect(formatForTsDoc("\n   this is an indented line")).to.be.eq("\n   this is an indented line");
+    expect(formatForTsDoc("\n   this is an indented line")).to.be.eq(
+      "\n   this is an indented line"
+    );
   });
 
   it("returns collapsed leading whitespace for 5 spaces in front", () => {
-      expect(formatForTsDoc("\n     this is an indented line")).to.be.eq("\n   this is an indented line");
+    expect(formatForTsDoc("\n     this is an indented line")).to.be.eq(
+      "\n   this is an indented line"
+    );
   });
 
   it("returns whitespace for 5 spaces in middle", () => {
-      expect(formatForTsDoc("\nthis is a spaced     line")).to.be.eq("\nthis is a spaced     line");
+    expect(formatForTsDoc("\nthis is a spaced     line")).to.be.eq(
+      "\nthis is a spaced     line"
+    );
   });
 });
