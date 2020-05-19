f7f9807832f1840b9d69ab7cc831f9692fd196e8
Daniel Barbato <dbarbato@salesforce.com>
2020-05-19 14:35:35 -0400

    Migrated integration-tests to integrationTest

---
+++ integrationTests/tests/authHelper.test.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+"use strict";
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { stripBearer } from "@commerce-apps/core";
+
+before(() => {
+  chai.use(chaiAsPromised);
+});
+
+describe("TS: Auth Helper Strip Bearer", () => {
+  it("Successfully strips the prefix of `Bearer ` from the supplied string", () => {
+    expect(stripBearer("Bearer 123456")).to.equal("123456");
+  });
+});
