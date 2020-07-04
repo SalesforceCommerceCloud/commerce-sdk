dfd10630fdcea9b53c7e131dbb5314909497e8cd
Jeff Albert <jalbert@salesforce.com>
2020-07-03 22:26:43 -0400

    Refactor to get metadata to client rendering

--- packages/generator/test/renderers/helpers.test.ts
+++ packages/generator/test/renderers/helpers.test.ts
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import * as renderer from "../../src/renderer";
-import { model, RestApi } from "@commerce-apps/raml-toolkit";
+import { RestApi } from "@commerce-apps/raml-toolkit";
 import { default as chai, expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import tmp from "tmp";
@@ -28,52 +28,6 @@ function makeApiFamily(): RestApi[] {
 }
 
 describe("Renderer Helpers", () => {
-  describe("sortApis", () => {
-    it("sorts APIs alphabetically", () => {
-      const api1: RestApi = {
-        name: "B",
-        id: "B",
-        groupId: "B",
-        assetId: "assignments"
-      };
-      const api2: RestApi = {
-        name: "A",
-        id: "A",
-        groupId: "A",
-        assetId: "assignments"
-      };
-      const api3: RestApi = {
-        name: "C",
-        id: "C",
-        groupId: "C",
-        assetId: "assignments"
-      };
-      const m = {} as model.domain.WebApi;
-      const apiFamilies: renderer.IApiClientsInfo[][] = [
-        [
-          { family: "Shopper", model: m, config: api1 },
-          { family: "Shopper", model: m, config: api2 },
-          { family: "Shopper", model: m, config: api3 }
-        ],
-        [
-          { family: "AI", model: m, config: api3 },
-          { family: "AI", model: m, config: api2 },
-          { family: "AI", model: m, config: api1 }
-        ]
-      ];
-
-      renderer.sortApis(apiFamilies);
-      expect(apiFamilies[0][0].family).to.equal("AI");
-      expect(apiFamilies[0][0].config.name).to.equal("A");
-      expect(apiFamilies[0][1].config.name).to.equal("B");
-      expect(apiFamilies[0][2].config.name).to.equal("C");
-      expect(apiFamilies[1][0].family).to.equal("Shopper");
-      expect(apiFamilies[1][0].config.name).to.equal("A");
-      expect(apiFamilies[1][1].config.name).to.equal("B");
-      expect(apiFamilies[1][2].config.name).to.equal("C");
-    });
-  });
-
   describe("loadApiConfig", () => {
     it("loads the API config file specified by the build config", () => {
       const json = { key: "value", nested: { property: 1 } };
@@ -98,7 +52,13 @@ describe("Renderer Helpers", () => {
       expect(models)
         .to.be.an("array")
         .with.lengthOf(1);
-      expect(models[0]).to.be.an.instanceOf(model.document.Document);
+      expect(models[0]).to.deep.include({
+        metadata: {
+          id: "Test",
+          assetId: "site",
+          fatRaml: { mainFile: "site.raml" }
+        }
+      });
     });
 
     it("throws an error when ID is missing", () => {
@@ -127,36 +87,7 @@ describe("Renderer Helpers", () => {
       expect(result[0][1])
         .to.be.an("array")
         .with.lengthOf(1);
-      expect(result[0][1][0]).to.be.an.instanceOf(model.document.Document);
-    });
-  });
-
-  describe("objectFromEntries", () => {
-    type Entries = [string, unknown][];
-
-    it("creates an empty object from an empty list of entries", () => {
-      expect(renderer.objectFromEntries([])).to.deep.equal({});
-    });
-
-    it("creates an object with keys/values matching the list of entries", () => {
-      const entries: Entries = [
-        ["one", 1],
-        ["two", 2]
-      ];
-      const object = { one: 1, two: 2 };
-      expect(renderer.objectFromEntries(entries)).to.deep.equal(object);
-    });
-
-    it("preserves references to object and function values", () => {
-      const nested = { property: "value" };
-      const method = (): null => null;
-      const result = renderer.objectFromEntries([
-        ["nested", nested],
-        ["method", method]
-      ] as Entries);
-      expect(result).to.deep.equal({ nested, method });
-      expect(result.nested).to.equal(nested);
-      expect(result.method).to.equal(method);
+      expect(result[0][1][0]).to.have.keys(["document", "metadata"]);
     });
   });
 });
