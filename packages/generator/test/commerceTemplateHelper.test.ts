0b1d7c52c465c9c642897cd73e2929355a9251a6
Daniel Barbato <dbarbato@salesforce.com>
2020-07-08 16:33:55 -0400

    Fixed formatting issue

--- packages/generator/test/commerceTemplateHelper.test.ts
+++ packages/generator/test/commerceTemplateHelper.test.ts
@@ -5,25 +5,25 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import sinon from 'sinon';
-import { expect } from 'chai';
-import { model, amf } from '@commerce-apps/raml-toolkit';
+import sinon from "sinon";
+import { expect } from "chai";
+import { model, amf } from "@commerce-apps/raml-toolkit";
 
 const {
-  getRequestPayloadTypeWithNamespace,
+  getRequestPayloadTypeWithNamespace
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-} = require('../src/commerceTemplateHelper');
+} = require("../src/commerceTemplateHelper");
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const commerceTemplateHelper = require('../src/templateHelpers');
+const commerceTemplateHelper = require("../src/templateHelpers");
 
-describe('Test retreval of types with namespace prefixes', () => {
+describe("Test retreval of types with namespace prefixes", () => {
   let singleResponse: model.domain.Request;
   let multiResponse: model.domain.Request;
 
   before(() => {
     amf.AMF.init();
-    console.log('arrayPayload');
+    console.log("arrayPayload");
     //Setup response with multiple types
     const arrayPayload = new model.domain.Payload().withSchema(
       new model.domain.ArrayShape()
@@ -35,40 +35,40 @@ describe('Test retreval of types with namespace prefixes', () => {
 
     //setup response with one type
 
-    console.log('singleResponse');
+    console.log("singleResponse");
     singleResponse = new model.domain.Request();
 
-    console.log('payload');
+    console.log("payload");
 
     singleResponse.withPayload();
 
-    console.log('getRequestPayloadTypeStub');
+    console.log("getRequestPayloadTypeStub");
     //setup stub
     sinon
-      .stub(commerceTemplateHelper, 'getRequestPayloadType')
+      .stub(commerceTemplateHelper, "getRequestPayloadType")
       .withArgs(multiResponse)
-      .returns('Array<Foo1 | Baa2>')
+      .returns("Array<Foo1 | Baa2>")
       .withArgs(singleResponse)
-      .returns('Bar1');
+      .returns("Bar1");
   });
 
   after(() => {
     sinon.restore();
   });
 
-  it('Single request payload type returns success', () => {
-    console.log('expect');
+  it("Single request payload type returns success", () => {
+    console.log("expect");
 
     expect(getRequestPayloadTypeWithNamespace(singleResponse))
-      .to.be.a('string')
-      .and.equal('types.Bar1');
+      .to.be.a("string")
+      .and.equal("types.Bar1");
   });
 
-  it('Multiple request payload type returns success', () => {
-    console.log('expect');
+  it("Multiple request payload type returns success", () => {
+    console.log("expect");
 
     expect(getRequestPayloadTypeWithNamespace(multiResponse))
-      .to.be.a('string')
-      .and.equal('Array<types.Foo1 | types.Baa2>');
+      .to.be.a("string")
+      .and.equal("Array<types.Foo1 | types.Baa2>");
   });
 });
