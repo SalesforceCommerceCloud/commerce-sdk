adc62139a2d3ebd804059f01112cda076a79f82d
Jeff Albert <jalbert@salesforce.com>
2020-07-22 13:08:33 -0400

    Added tests for type import

---
+++ testIntegration/tests/import.test.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+"use strict";
+import chai from "chai";
+import { expectType } from "tsd";
+import { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import nock from "nock";
+import { Customer } from "commerce-sdk";
+
+before(() => {
+  chai.use(chaiAsPromised);
+});
+
+describe("TS: Import works successfully", () => {
+  afterEach(nock.cleanAll);
+
+  it("Import works", async () => {
+    return expect(import("commerce-sdk")).to.eventually.contain.keys([
+      "helpers",
+      "sdkLogger",
+    ]);
+  });
+
+  it("Type import works", async () => {
+    // This type was selected for its simplicity
+    expectType<Customer.ShopperCustomers.BasketsResult>({
+      baskets: [],
+      total: 0,
+    });
+  });
+});
