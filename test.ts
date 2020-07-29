3b01e0f53696987aea1f65c2d6e7ff57dc84d0e2
Jeff Albert <jalbert@salesforce.com>
2020-07-29 12:19:09 -0400

    Saving changes for demo

--- test.ts
+++ test.ts
@@ -4,21 +4,80 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+///// ALL SHOULD BE ONE IMPORT!!!!
 import { common, getBearer } from "@commerce-apps/raml-toolkit";
-import fs from "fs-extra";
 import {
   searchExchange,
   downloadRestApis,
 } from "@commerce-apps/raml-toolkit/lib/download/exchangeDownloader";
-import { renderTemplates } from "./src/renderer";
-import { indexTemplate } from "./src/handlebarsConfig";
+import { ApiTree } from "@commerce-apps/raml-toolkit/lib/common/structures/apiTree";
+import {
+  registerPartial,
+  HandlebarsWithAmfHelpers as Handlebars,
+} from "@commerce-apps/raml-toolkit/lib/generate/index";
 
-async function createSdk() {
-  const apis = await common.createApiTree("./newApis");
-  fs.ensureDirSync("newPipeline");
-  fs.writeFileSync("newPipeline/index.ts", indexTemplate(apis));
+import path from "path";
+
+const templateDirectory = "templates";
+
+//////// HELPER REGISTRATION ////////
+import * as helpers from "./src/templateHelpers";
+
+function registerHelpers(): void {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("handlebars-helpers")({ handlebars: Handlebars });
+
+  for (const helper of Object.keys(helpers)) {
+    Handlebars.registerHelper(helper, helpers[helper]);
+  }
 }
 
+/////// PARTIAL REGISTRATION //////
+function registerPartials(): void {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  registerPartial(
+    "dtoPartial",
+    path.join(templateDirectory, "dtoPartial.ts.hbs")
+  );
+  registerPartial(
+    "operationsPartial",
+    path.join(templateDirectory, "operations.ts.hbs")
+  );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function setupTemplates(apis: ApiTree): any {
+  apis.addTemplate("templates/index.ts.hbs", "newPipeline/index.ts");
+
+  apis.children.forEach((child) => {
+    child.addTemplate(
+      "templates/apiFamily.ts.hbs",
+      `newPipeline/${child.name.lowerCamelCase}/index.ts`
+    );
+    child.children.forEach((api) => {
+      api.addTemplate(
+        "templates/ClientInstance.ts.hbs",
+        path.join(
+          "newPipeline",
+          child.name.lowerCamelCase,
+          api.name.lowerCamelCase,
+          `${api.name.lowerCamelCase}.ts`
+        )
+      );
+    });
+  });
+  return apis;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+async function createSdk() {
+  let apis = await common.createApiTree("./newApis");
+  apis = setupTemplates(apis);
+  apis.renderAll();
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function updateApis(apiFamily) {
   const token = await getBearer(
     process.env.ANYPOINT_USERNAME,
@@ -37,6 +96,7 @@ async function updateApis(apiFamily) {
 // updateApis("search");
 // updateApis("product");
 // updateApis("cdn");
-// updateApis("seller");
 
+registerHelpers();
+registerPartials();
 createSdk();
