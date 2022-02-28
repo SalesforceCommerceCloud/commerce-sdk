83ea03af80338d092690e529e88404bd193db000
Joel Uong <joel.uong@salesforce.com>
2022-02-28 15:24:41 -0500

    Lint code

--- src/generate.ts
+++ src/generate.ts
@@ -7,14 +7,17 @@
 
 import path from "path";
 import { generate } from "@commerce-apps/raml-toolkit";
-import { copySync } from 'fs-extra';
+import { copySync } from "fs-extra";
 import { registerHelpers, registerPartials, setupApis } from "./lib/utils";
 
 const API_DIRECTORY = path.resolve(
   process.env.COMMERCE_SDK_INPUT_DIR || `${__dirname}/../apis`
 );
-const OUTPUT_DIRECTORY = path.join(__dirname, '../renderedTemplates/slasHelper');
-const STATIC_DIRECTORY = path.join(__dirname, './slasHelper');
+const OUTPUT_DIRECTORY = path.join(
+  __dirname,
+  "../renderedTemplates/slasHelper"
+);
+const STATIC_DIRECTORY = path.join(__dirname, "./slasHelper");
 
 registerHelpers();
 registerPartials();
@@ -22,7 +25,7 @@ registerPartials();
 console.log(`Creating SDK for ${API_DIRECTORY}`);
 
 const skipTestFiles = (src: string): boolean => !/\.test\.[a-z]+$/.test(src);
-copySync(STATIC_DIRECTORY, OUTPUT_DIRECTORY, {filter: skipTestFiles});
+copySync(STATIC_DIRECTORY, OUTPUT_DIRECTORY, { filter: skipTestFiles });
 
 setupApis(
   API_DIRECTORY,
--- src/slasHelper/slasHelper.ts
+++ src/slasHelper/slasHelper.ts
@@ -5,23 +5,29 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import {nanoid} from 'nanoid';
-import {ShopperLogin} from '../../renderedTemplates/customer/shopperLogin/shopperLogin';
+import { nanoid } from "nanoid";
+import { ShopperLogin } from "../../renderedTemplates/customer/shopperLogin/shopperLogin";
 
-export const stringToBase64 = (unencoded: string): string => Buffer.from(unencoded).toString('base64');
+/**
+ * Converts a string into Base64 encoding
+ * @param unencoded - A string to be encoded
+ * @returns Base64 encoded string
+ */
+export const stringToBase64 = (unencoded: string): string =>
+  Buffer.from(unencoded).toString("base64");
 
 /**
  * Parse out the code and usid from a redirect url
- * @param urlString A url that contains `code` and `usid` query parameters, typically returned when calling a Shopper Login endpoint
+ * @param urlString - A url that contains `code` and `usid` query parameters, typically returned when calling a Shopper Login endpoint
  * @returns An object containing the code and usid.
  */
 export const getCodeAndUsidFromUrl = (
   urlString: string
-): {code: string; usid: string} => {
+): { code: string; usid: string } => {
   const url = new URL(urlString);
   const urlParams = new URLSearchParams(url.search);
-  const usid = urlParams.get('usid') ?? '';
-  const code = urlParams.get('code') ?? '';
+  const usid = urlParams.get("usid") ?? "";
+  const code = urlParams.get("code") ?? "";
 
   return {
     code,
@@ -37,31 +43,31 @@ export const createCodeVerifier = (): string => nanoid(128);
 
 /**
  * Encodes a code verifier to a code challenge to send to the authorization endpoint
- * @param codeVerifier random string to use as a code verifier
+ * @param codeVerifier - random string to use as a code verifier
  * @returns code challenge
  */
 export const generateCodeChallenge = async (
   codeVerifier: string
 ): Promise<string> => {
-    const urlSafe = (input: string) =>
-        input.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+  const urlSafe = (input: string) =>
+    input.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
 
-    const crypto = await import('crypto');
-    const challenge = urlSafe(
-        crypto.default.createHash('sha256').update(codeVerifier).digest('base64')
-    );
+  const crypto = await import("crypto");
+  const challenge = urlSafe(
+    crypto.default.createHash("sha256").update(codeVerifier).digest("base64")
+  );
 
-    /* istanbul ignore next */
-    if (challenge.length === 0) {
-    throw new Error('Problem generating code challenge');
-    }
+  /* istanbul ignore next */
+  if (challenge.length === 0) {
+    throw new Error("Problem generating code challenge");
+  }
 
-    return challenge;
+  return challenge;
 };
 
 /**
  * Wrapper for the authorization endpoint. For federated login (3rd party IDP non-guest), the caller should redirect the user to the url in the url field of the returned object. The url will be the login page for the 3rd party IDP and the user will be sent to the redirectURI on success. Guest sessions return the code and usid directly with no need to redirect.
- * @param slasClient a configured instance of the ShopperLogin SDK client
+ * @param slasClient - a configured instance of the ShopperLogin SDK client
  * @param codeVerifier - random string created by client app to use as a secret in the request
  * @param parameters - Request parameters used by the `authorizeCustomer` endpoint.
  * @param parameters.redirectURI - the location the client will be returned to after successful login with 3rd party IDP. Must be registered in SLAS.
@@ -77,7 +83,7 @@ export async function authorize(
     hint?: string;
     usid?: string;
   }
-): Promise<{code: string; url: string; usid: string}> {
+): Promise<{ code: string; url: string; usid: string }> {
   const codeChallenge = await generateCodeChallenge(codeVerifier);
 
   // Create a copy to override specific fetchOptions
@@ -98,30 +104,30 @@ export async function authorize(
     parameters: {
       client_id: slasClient.clientConfig.parameters.clientId,
       code_challenge: codeChallenge,
-      ...(parameters.hint && {hint: parameters.hint}),
+      ...(parameters.hint && { hint: parameters.hint }),
       organizationId: slasClient.clientConfig.parameters.organizationId,
       redirect_uri: parameters.redirectURI,
-      response_type: 'code',
-      ...(parameters.usid && {usid: parameters.usid}),
+      response_type: "code",
+      ...(parameters.usid && { usid: parameters.usid }),
     },
   };
 
   const response = await slasClientCopy.authorizeCustomer(options, true);
-  const redirectUrl = response.headers?.get('location') || response.url;
+  const redirectUrl = response.headers?.get("location") || response.url;
 
   // url is a read only property we unfortunately cannot mock out using nock
   // meaning redirectUrl will not have a falsy value for unit tests
   /* istanbul ignore next */
   if (!redirectUrl) {
-    throw new Error('Authorization failed');
+    throw new Error("Authorization failed");
   }
 
-  return {url: redirectUrl, ...getCodeAndUsidFromUrl(redirectUrl)};
+  return { url: redirectUrl, ...getCodeAndUsidFromUrl(redirectUrl) };
 }
 
 /**
  * A single function to execute the ShopperLogin Public Client Guest Login with proof key for code exchange flow as described in the [API documentation](https://developer.salesforce.com/docs/commerce/commerce-api/references?meta=shopper-login-and-api-access:Summary).
- * @param slasClient a configured instance of the ShopperLogin SDK client.
+ * @param slasClient - a configured instance of the ShopperLogin SDK client.
  * @param parameters - parameters to pass in the API calls.
  * @param parameters.redirectURI - Per OAuth standard, a valid app route. Must be listed in your SLAS configuration. On server, this will not be actually called. On browser, this will be called, but ignored.
  * @param parameters.usid? - Unique Shopper Identifier to enable personalization.
@@ -138,25 +144,25 @@ export async function loginGuestUser(
 
   const authResponse = await authorize(slasClient, codeVerifier, {
     redirectURI: parameters.redirectURI,
-    hint: 'guest',
-    ...(parameters.usid && {usid: parameters.usid}),
+    hint: "guest",
+    ...(parameters.usid && { usid: parameters.usid }),
   });
 
   const tokenBody: ShopperLogin.TokenRequest = {
     client_id: slasClient.clientConfig.parameters.clientId,
     code: authResponse.code,
     code_verifier: codeVerifier,
-    grant_type: 'authorization_code_pkce',
+    grant_type: "authorization_code_pkce",
     redirect_uri: parameters.redirectURI,
     usid: authResponse.usid,
   };
 
-  return slasClient.getAccessToken({body: tokenBody});
+  return slasClient.getAccessToken({ body: tokenBody });
 }
 
 /**
  * A single function to execute the ShopperLogin Public Client Registered User B2C Login with proof key for code exchange flow as described in the [API documentation](https://developer.salesforce.com/docs/commerce/commerce-api/references?meta=shopper-login-and-api-access:Summary).
- * @param slasClient a configured instance of the ShopperLogin SDK client.
+ * @param slasClient - a configured instance of the ShopperLogin SDK client.
  * @param credentials - the id and password to login with.
  * @param credentials.username - the id of the user to login with.
  * @param credentials.password - the password of the user to login with.
@@ -195,7 +201,7 @@ export async function loginRegisteredUserB2C(
       client_id: slasClient.clientConfig.parameters.clientId,
       code_challenge: codeChallenge,
       channel_id: slasClient.clientConfig.parameters.siteId,
-      ...(parameters.usid && {usid: parameters.usid}),
+      ...(parameters.usid && { usid: parameters.usid }),
     },
   };
 
@@ -207,45 +213,45 @@ export async function loginRegisteredUserB2C(
     client_id: slasClient.clientConfig.parameters.clientId,
     code: authResponse.code,
     code_verifier: codeVerifier,
-    grant_type: 'authorization_code_pkce',
+    grant_type: "authorization_code_pkce",
     organizationId: slasClient.clientConfig.parameters.organizationId,
     redirect_uri: parameters.redirectURI,
     usid: authResponse.usid,
   };
 
-  return slasClient.getAccessToken({body: tokenBody});
+  return slasClient.getAccessToken({ body: tokenBody });
 }
 
 /**
  * Exchange a refresh token for a new access token.
- * @param slasClient a configured instance of the ShopperLogin SDK client.
+ * @param slasClient - a configured instance of the ShopperLogin SDK client.
  * @param parameters - parameters to pass in the API calls.
  * @param parameters.refreshToken - a valid refresh token to exchange for a new access token (and refresh token).
  * @returns TokenResponse
  */
 export function refreshAccessToken(
   slasClient: ShopperLogin,
-  parameters: {refreshToken: string}
+  parameters: { refreshToken: string }
 ): Promise<ShopperLogin.TokenResponse> {
   const body = {
-    grant_type: 'refresh_token',
+    grant_type: "refresh_token",
     refresh_token: parameters.refreshToken,
     client_id: slasClient.clientConfig.parameters.clientId,
   };
 
-  return slasClient.getAccessToken({body});
+  return slasClient.getAccessToken({ body });
 }
 
 /**
  * Logout a shopper. The shoppers access token and refresh token will be revoked and if the shopper authenticated with ECOM the OCAPI JWT will also be revoked.
- * @param slasClient a configured instance of the ShopperLogin SDK client.
+ * @param slasClient - a configured instance of the ShopperLogin SDK client.
  * @param parameters - parameters to pass in the API calls.
  * @param parameters.refreshToken - a valid refresh token to exchange for a new access token (and refresh token).
  * @returns TokenResponse
  */
 export function logout(
   slasClient: ShopperLogin,
-  parameters: {refreshToken: string}
+  parameters: { refreshToken: string }
 ): Promise<ShopperLogin.TokenResponse> {
   return slasClient.logoutCustomer({
     parameters: {
