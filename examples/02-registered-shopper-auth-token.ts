7e33a34a41e174bc8ddc6adfb176286b94f70c71
Gaurav Khunger <gkhunger@salesforce.com>
2022-03-11 11:46:27 -0500

    Cast response to TokenResponse

--- examples/02-registered-shopper-auth-token.ts
+++ examples/02-registered-shopper-auth-token.ts
@@ -8,22 +8,23 @@
 /**
  * Get authorization token for a registered customer
  * Usage: ts-node examples/02-registered-shopper-auth-token.ts
- * Note: Replace configuration parameters and client credentials before running
  */
 import { ClientConfig, Customer } from "../dist";
-import { getObjectFromResponse } from "@commerce-apps/core";
 
 // client credentials
 const CLIENT_ID = "da422690-7800-41d1-8ee4-3ce983961078";
 const CLIENT_SECRET = "D*HHUrgO2%qADp2JTIUi";
+const ORG_ID = "f_ecom_zzte_053";
+const SHORT_CODE = "kv7kzm78";
+const SITE_ID = "RefArch";
 
 // client configuration parameters
 const clientConfig: ClientConfig = {
   parameters: {
     clientId: CLIENT_ID,
-    organizationId: "f_ecom_zzte_053",
-    shortCode: "kv7kzm78",
-    siteId: "RefArch",
+    organizationId: ORG_ID,
+    shortCode: SHORT_CODE,
+    siteId: SITE_ID,
   },
 };
 
@@ -32,30 +33,27 @@ const clientConfig: ClientConfig = {
  *
  * @returns authorization token
  */
-async function getRegisteredShopperToken(): Promise<string> {
+async function getRegisteredShopperToken(): Promise<Customer.ShopperLogin.TokenResponse> {
   const credentials = `${CLIENT_ID}:${CLIENT_SECRET}`;
   const base64data = Buffer.from(credentials).toString("base64");
   const headers = { Authorization: `Basic ${base64data}` };
   const client = new Customer.ShopperLogin(clientConfig);
 
-  const response = await client.getAccessToken(
-    {
+  const response: Customer.ShopperLogin.TokenResponse =
+    await client.getAccessToken({
       headers,
       body: {
-        /* eslint-disable-next-line @typescript-eslint/camelcase */
         grant_type: "client_credentials",
       },
-    },
-    true
-  );
+    });
 
-  const responseObject: any = await getObjectFromResponse(response);
-
-  return responseObject.access_token;
+  return response;
 }
 
 getRegisteredShopperToken()
-  .then((shopperToken) => console.log(`Authorization token: ${shopperToken}`))
+  .then((shopperToken) =>
+    console.log(`Authorization token: ${shopperToken.access_token}`)
+  )
   .catch((error) => {
     console.log(`Error fetching token for registered customer: ${error}`);
   });
