65d9a7789a2bea8e5ab6a36aad8c83be2a299707
Gaurav Khunger <gkhunger@salesforce.com>
2022-03-15 14:29:22 -0400

    Replace Shopper token flow with SLAS for client creds (#357)
    * Replace Shopper token flow with SLAS for client creds
    
    * Add return type to getRegisteredShopperToken
    
    * Disable camelcase rule for grant_type
    
    * Cast response to TokenResponse
    
    * Update examples/02-registered-shopper-auth-token.ts
    
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>
    
    * Add package.json
    
    * Move the example to guest user flow
    
    * Formatting fix and error message update
    
    * Delete package-lock.json
    
    * Update error message
    
    * Restore package lock
    
    * Update examples/01-guest-shopper-auth-token.ts
    
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>
    
    * Update examples/01-guest-shopper-auth-token.ts
    
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>
    
    * Update README
    
    * Readme updates
    
    * Readme updates
    
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>

--- examples/01-guest-shopper-auth-token.ts
+++ examples/01-guest-shopper-auth-token.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, salesforce.com, inc.
+ * Copyright (c) 2022, salesforce.com, inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -8,36 +8,51 @@
 /**
  * Get authorization token for a guest user
  * Usage: ts-node examples/01-guest-shopper-auth-token.ts
- * Note: Replace configuration parameters before running
+ * For more information, see (Shopper Login and API Access Service)[https://developer.commercecloud.com/s/api-details/a003k00000VWfNDAA1/commerce-cloud-developer-centershopperloginandapiaccessservice].
  */
-import { helpers, ClientConfig } from "../dist";
+import { ClientConfig, Customer } from "commerce-sdk";
 
-//client configuration parameters
+// demo client credentials, if you have access to your own please replace them below.
+// The client secret should not be stored in plain text alongside code. Please store the secret in a secure location.
+const CLIENT_ID = "da422690-7800-41d1-8ee4-3ce983961078";
+const CLIENT_SECRET = "D*HHUrgO2%qADp2JTIUi";
+const ORG_ID = "f_ecom_zzte_053";
+const SHORT_CODE = "kv7kzm78";
+const SITE_ID = "RefArch";
+
+// client configuration parameters
 const clientConfig: ClientConfig = {
   parameters: {
-    clientId: "your-client-id",
-    organizationId: "your-org-id",
-    shortCode: "your-short-code",
-    siteId: "your-site-id",
+    clientId: CLIENT_ID,
+    organizationId: ORG_ID,
+    shortCode: SHORT_CODE,
+    siteId: SITE_ID,
   },
 };
 
 /**
- * Invoke helper function to retrieve the authorization token for the guest user
- * Doc: https://salesforcecommercecloud.github.io/commerce-sdk/modules/_helpers_.html#getshoppertoken
+ * Get the shopper or guest JWT/access token, along with a refresh token, using client credentials
+ *
+ * @returns guest user authorization token
  */
-helpers
-  .getShopperToken(clientConfig, { type: "guest" })
-  .then((shopperToken) => {
-    console.log(`Authorization token: ${shopperToken.getAuthToken()}`);
-    console.log(
-      `Customer Info: ${JSON.stringify(
-        shopperToken.getCustomerInfo(),
-        null,
-        2
-      )}`
-    );
-  })
+async function getGuestUserAuthToken(): Promise<Customer.ShopperLogin.TokenResponse> {
+  const credentials = `${CLIENT_ID}:${CLIENT_SECRET}`;
+  const base64data = Buffer.from(credentials).toString("base64");
+  const headers = { Authorization: `Basic ${base64data}` };
+  const client = new Customer.ShopperLogin(clientConfig);
+
+  return await client.getAccessToken({
+    headers,
+    body: {
+      grant_type: "client_credentials",
+    },
+  });
+}
+
+getGuestUserAuthToken()
+  .then((shopperToken) =>
+    console.log(`Authorization token: ${shopperToken.access_token}`)
+  )
   .catch((error) => {
-    console.log(`Error fetching token: ${error}`);
+    console.log(`Error fetching token for the guest user: ${error}`);
   });
